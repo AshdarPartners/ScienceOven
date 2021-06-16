b9a5f5b13601e953ed12ede046c1b8a5f2bf13ac
Darin Strait <Darin.Strait@ashdar-partners.com>
2021-06-16 14:50:42 -0400

    PSM1 now handles empty .\Private

--- ScienceOven/ScienceOven.psm1
+++ ScienceOven/ScienceOven.psm1
@@ -1,6 +1,13 @@
 # Dot source public/private functions
-$public  = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
-$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
+
+$PublicPath = Join-Path -Path $PSScriptRoot -ChildPath 'Public'
+$public  = @(Get-ChildItem -Path $PublicPath -filter '*.ps1'  -Recurse -ErrorAction Stop)
+
+# We might not actually have an Private functions, so be careful
+
+$PrivatePath = Join-Path -Path $PSScriptRoot -ChildPath 'Private'
+$private = @(Get-ChildItem -Path $PrivatePath -filter '*.ps1' -Recurse -ErrorAction Stop)
+
 foreach ($import in @($public + $private)) {
     try {
         . $import.FullName
