792f7450c51b1563a3213adced750106d809a11c
Darin Strait <Darin.Strait@ashdar-partners.com>
2021-11-20 19:16:30 -0500

    Uncommit the Import-Module line

--- Tests/Get-SqlServerAgent.Tests.ps1
+++ Tests/Get-SqlServerAgent.Tests.ps1
@@ -12,7 +12,7 @@ BeforeDiscovery {
     $Path = Join-Path -Path $Path -ChildPath $moduleName
     $Path = Join-Path -Path $Path -ChildPath ($moduleName + '.psd1')
 
-    # $ModuleInfo = Import-Module -Name $Path -Force -PassThru
+    $ModuleInfo = Import-Module -Name $Path -Force -PassThru
 
     $SubjectArea = (Split-Path -Path $PSCommandPath -Leaf) -Replace 'Get\-','' -Replace '.Tests.ps1',''
 }
